e490b1f356acba5cc23c7d72a892635dd231ec8f
Victor Moncada <victor.moncada@cea.fr>
2026-01-06 16:11:04 +0100

    Correct error with clang

--- stenos/internal/block_compress.h
+++ stenos/internal/block_compress.h
@@ -1100,7 +1100,7 @@ namespace stenos
 #endif
 
 
-	static inline size_t block_guess_compress_size(const void* __src, size_t bytesoftype, size_t bytes, size_t max_bytes) noexcept 
+	inline size_t block_guess_compress_size(const void* __src, size_t bytesoftype, size_t bytes, size_t max_bytes) noexcept 
 	{
 #ifdef __SSE4_1__
 		if ((cpu_features().HAS_SSE41)) {
